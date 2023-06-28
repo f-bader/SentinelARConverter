d8524bbcf5408f47cb4d2d236b25c35ae0421953
Christian Piet <43993129+Manbearpiet@users.noreply.github.com>
2023-06-28 13:09:07 +0000

    Code changes

--- src/public/Convert-SentinelARArmToYaml.ps1
+++ src/public/Convert-SentinelARArmToYaml.ps1
@@ -75,30 +75,45 @@ function Convert-SentinelARArmToYaml {
         [Parameter(ParameterSetName = 'UseOriginalFilename')]
         [switch]$UseOriginalFilename,
 
+        [Parameter(ParameterSetName = 'Pipeline')]
         [Parameter(ParameterSetName = 'UseDisplayNameAsFilename')]
         [switch]$UseDisplayNameAsFilename,
 
+        [Parameter(ParameterSetName = 'Pipeline')]
         [Parameter(ParameterSetName = 'UseIdAsFilename')]
-        [switch]$UseIdAsFilename
+        [switch]$UseIdAsFilename,
+
+        [Parameter(ParameterSetName = 'Pipeline')]
+        [string]$Directory,
+
+        [Parameter(
+            ParameterSetName = 'Path')]
+        [Parameter(
+            ParameterSetName = 'UseOriginalFilename')]
+        [Parameter(
+            ParameterSetName = 'UseDisplayNameAsFilename')]
+        [Parameter(
+            ParameterSetName = 'UseIdAsFilename')]
+        [Parameter(
+            ParameterSetName = 'StdOut')]
+        [switch]$Force = $false
     )
 
+    process {
 
-    begin {
+        #region common
+
         if ($PsCmdlet.ParameterSetName -ne "Pipeline" ) {
-            if (-not (Test-Path $Filename) ) {
+            try {
+                if (-not (Test-Path $Filename)) {
+                    Write-Error -Exception
+                }
+            } catch {
                 throw "File not found"
             }
         }
-    }
 
-    process {
-        # Use pipeline data and create a variable containing all parsed strings
-        if ($PsCmdlet.ParameterSetName -eq "Pipeline") {
-            $FullARM += $Data
-        }
-    }
 
-    end {
         # Mapping of Arm property names to YAML when different
         $ValueNameMappingArm2Yaml = [ordered]@{
             "displayName"           = "name"
@@ -106,7 +121,7 @@ function Convert-SentinelARArmToYaml {
             "templateVersion"       = "version"
             "techniques"            = "relevantTechniques"
         }
-
+        
         # Mapping of Arm operator names to YAML when different
         $CompareOperatorArm2Yaml = @{
             "Equals"             = "eq"
@@ -115,12 +130,12 @@ function Convert-SentinelARArmToYaml {
             "LessThan"           = "lt"
             "LessThanOrEqual"    = "le"
         }
-
+        
         # List of values to always remove
         $RemoveArmValues = @(
             "enabled"
         )
-
+        
         $DefaultSortOrderInYAML = @(
             "id",
             "name",
@@ -138,106 +153,165 @@ function Convert-SentinelARArmToYaml {
             "query"
         )
 
+
+        # Use pipeline data and create a variable containing all parsed strings
+        if ($PsCmdlet.ParameterSetName -eq "Pipeline") {
+            $FullARM += $Data
+        }
+
         # Use parsed pipeline data if no file was specified (default)
         try {
             if ($PsCmdlet.ParameterSetName -eq "Pipeline") {
-                $AnalyticsRuleTemplate = $FullARM | ConvertFrom-Json -Verbose
+                $AnalyticsRuleTemplate = $FullARM | ConvertFrom-Json -Depth 99 -Verbose
             } else {
                 Write-Verbose "Read file `"$Filename`""
-                $AnalyticsRuleTemplate = Get-Content $Filename | ConvertFrom-Json -Verbose
+                $AnalyticsRuleTemplate = Get-Content $Filename | ConvertFrom-Json -Depth 99 -Verbose
             }
         } catch {
             throw "Could not convert source file. JSON might be corrupted"
-        }
+        }    
 
-        if ($AnalyticsRuleTemplate.resources.Count -ne 1) {
-            throw "ARM template must contain exactly one resource"
-        }
-
-        # Get the id of the analytic rule
-        if ($AnalyticsRuleTemplate.resources.id -match "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}") {
-            $Id = $Matches[0]
-        } else {
-            Write-Warning "Error reading current Id. Generating new Id."
-            $Id = (New-Guid).Guid
-        }
-
-        Write-Verbose "Convert Analytics Rule $($AnalyticsRuleTemplate.resources.properties.displayName) ($($Id)) to YAML file"
-
-        #region Set output filename to defined value if not specified by user
-        if ($PsCmdlet.ParameterSetName -in ("UseOriginalFilename", "UseDisplayNameAsFilename", "UseIdAsFilename") ) {
-            $FileObject = Get-ChildItem $Filename
-            if ($UseOriginalFilename) {
-                # Use original filename as new filename
-                $NewFileName = $FileObject.Name -replace $FileObject.Extension, ".yaml"
-            }
-            if ($UseDisplayNameAsFilename) {
-                # Use the display name of the Analytics Rule as filename
-                $NewFileName = $AnalyticsRuleTemplate.resources.properties.displayName -Replace '[^0-9A-Z]', ' '
-                # Convert To CamelCase
-                $NewFileName = ((Get-Culture).TextInfo.ToTitleCase($NewFileName) -Replace ' ') + '.yaml'
-            }
-            if ($UseIdAsFilename) {
-                # Use id as of the Analytics Rule filename
-                $NewFileName = $Id + '.yaml'
-            }
-            $OutFile = Join-Path $FileObject.Directory $NewFileName
-        }
-        #endregion
-
-        # Get the properties of the analytic rule
-        $AnalyticsRule = $AnalyticsRuleTemplate.resources | Select-Object -ExpandProperty properties
-        # Add the id and kind from the ARM template
-        $AnalyticsRule = $AnalyticsRule | Add-Member -MemberType NoteProperty -Name "id" -Value $Id -PassThru -Force
-        $AnalyticsRule = $AnalyticsRule | Add-Member -MemberType NoteProperty -Name "kind" -Value $AnalyticsRuleTemplate.resources.kind -PassThru -Force
-        # Add version if not present
-        if ( [string]::IsNullOrWhiteSpace($AnalyticsRuleTemplate.resources.properties.templateVersion) ) {
-            $AnalyticsRule = $AnalyticsRule | Add-Member -MemberType NoteProperty -Name "version" -Value "1.0.0" -PassThru -Force
-        }
-        # Remove values that are not needed
-        foreach ($RemoveArmValue in $RemoveArmValues) {
-            $AnalyticsRule.PSObject.Properties.Remove($RemoveArmValue) | Out-Null
-        }
-
-        $JSON = $AnalyticsRule | ConvertTo-Json -Depth 100
-        # Use ISO8601 format for timespan values
-        $JSON = $JSON -replace '"PT([0-9]+)M"', '"$1m"' -replace '"PT([0-9]+)H"', '"$1h"' -replace '"P([0-9]+)D"', '"$1d"'
-
-        # Convert the names of the properties to the names used in the YAML
-        foreach ($Arm2Yaml in $ValueNameMappingArm2Yaml.Keys) {
-            $JSON = $JSON -replace $Arm2Yaml, $ValueNameMappingArm2Yaml[$Arm2Yaml]
-        }
-
-        # Convert the compare operators to the names used in the YAML
-        foreach ($Arm2Yaml in $CompareOperatorArm2Yaml.Keys) {
-            $JSON = $JSON -replace $Arm2Yaml, $CompareOperatorArm2Yaml[$Arm2Yaml]
-        }
-
-        # Convert the JSON to a PowerShell object
-        $AnalyticsRule = $JSON | ConvertFrom-Json
-
-        # Use custom sort order of YAML
-        $ErrorActionPreference = "SilentlyContinue"
-        $AnalyticsRuleKeys = $AnalyticsRule.PSObject.Properties.Name | Sort-Object { $i = $DefaultSortOrderInYAML.IndexOf($_) ; if ( $i -eq -1 ) { 100 } else { $i } }
-        $ErrorActionPreference = "Continue"
-        # Create ordered hashtable
-        $AnalyticsRuleCleaned = [ordered]@{}
-        foreach ($PropertyName in $AnalyticsRuleKeys) {
-            # Remove empty properties
-            if ( -not [string]::IsNullOrWhiteSpace($AnalyticsRule.$PropertyName) -or ( $AnalyticsRule.$PropertyName -is [array] -and ($AnalyticsRule.$PropertyName.Count -gt 0) ) ) {
-                $AnalyticsRuleCleaned.Add($PropertyName, $AnalyticsRule.$PropertyName)
+        try {
+            if ((-not $AnalyticsRuleTemplate.resources) -or (($AnalyticsRuleTemplate.resources).Count -lt 1)) {
+                throw "This template contains no Analytics Rules"
             }
         }
+        catch {
+            $PSCmdlet.ThrowTerminatingError($_)
+        }
 
-        # Convert the PowerShell object to YAML
-        $AnalyticsRuleYAML = $AnalyticsRuleCleaned | ConvertTo-Yaml
+        try {
+            if ($AnalyticsRuleTemplate.resources.type -ne "Microsoft.OperationalInsights/workspaces/providers/alertRules") {
+                throw "This template contains resources other than Analytics Rules"
+            }
+        }
+        catch {
+            $PSCmdlet.ThrowTerminatingError($_)
+        }
 
-        # Write the YAML to a file or return the YAML
-        if ($OutFile) {
-            $AnalyticsRuleYAML | Out-File $OutFile -Force -Encoding utf8
-            Write-Verbose "Output written to file: `"$OutFile`""
-        } else {
-            return $AnalyticsRuleYAML
+        #endregion common
+
+        #region ART
+        $resourceCounter = 0
+
+        foreach ($resource in $AnalyticsRuleTemplate.resources) {
+
+            # Get the id of the analytic rule
+
+            if ($resource.id -match "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}") {
+                $Id = $Matches[0]
+            } else {
+                Write-Warning "Error reading current Id. Generating new Id."
+                $Id = (New-Guid).Guid
+            }
+        
+            Write-Verbose "Convert Analytics Rule: $($resource.properties.displayName) ($($Id)) to YAML file"
+        
+            #region Set output filename to defined value if not specified by user
+            if ($PsCmdlet.ParameterSetName -in ("UseOriginalFilename", "UseDisplayNameAsFilename", "UseIdAsFilename") ) {
+                $FileObject = Get-ChildItem $Filename
+                if ($UseOriginalFilename) {
+                    # Use original filename as new filename
+                    if ($resourceCounter -eq 0) {
+                        $NewFileName = $FileObject.Name -replace $FileObject.Extension, ".yaml"
+                    } else {
+                        $NewFileName = $FileObject.BaseName + "_$resourceCounter" + ".yaml"
+                    }
+                }
+                if ($UseDisplayNameAsFilename) {
+                    # Use the display name of the Analytics Rule as filename
+                    $NewFileName = $resource.properties.displayName -Replace '[^0-9A-Z]', ' '
+                    # Convert To CamelCase
+                    $NewFileName = ((Get-Culture).TextInfo.ToTitleCase($NewFileName) -Replace ' ') + '.yaml'
+                }
+                if ($UseIdAsFilename) {
+                    # Use id as of the Analytics Rule filename
+                    $NewFileName = $Id + '.yaml'
+                }
+                
+                $OutFile = Join-Path $FileObject.Directory $NewFileName
+            } elseif ($PsCmdlet.ParameterSetName -in ("Path") -or ($PsCmdlet.ParameterSetName -in ("Pipeline") -and $OutFile -and -not $Directory)) {
+                if ($resourceCounter -gt 0) {
+                    $OutFile = $OutFile -replace ".yaml", "_$resourceCounter.yaml"
+                }
+            } elseif ($PsCmdlet.ParameterSetName -in ("Pipeline") -and ($UseDisplayNameAsFilename -or $UseIdAsFilename)) {
+                if ($UseDisplayNameAsFilename) {
+                    # Use the display name of the Analytics Rule as filename
+                    $NewFileName = $resource.properties.displayName -Replace '[^0-9A-Z]', ' '
+                    # Convert To CamelCase
+                    $NewFileName = ((Get-Culture).TextInfo.ToTitleCase($NewFileName) -Replace ' ') + '.yaml'
+                }
+                if ($UseIdAsFilename) {
+                    # Use id as of the Analytics Rule filename
+                    $NewFileName = $Id + '.yaml'
+                }
+                $OutFile = Join-Path -Path $Directory -ChildPath $NewFileName
+            }
+            #endregion
+        
+            # Get the properties of the analytic rule
+            $AnalyticsRule = $resource | Select-Object -ExpandProperty properties
+            # Add the id and kind from the ARM template
+            $AnalyticsRule = $AnalyticsRule | Add-Member -MemberType NoteProperty -Name "id" -Value $Id -PassThru -Force
+            $AnalyticsRule = $AnalyticsRule | Add-Member -MemberType NoteProperty -Name "kind" -Value $AnalyticsRuleTemplate.resources.kind -PassThru -Force
+            # Add version if not present
+            if ( [string]::IsNullOrWhiteSpace($AnalyticsRuleTemplate.resources.properties.templateVersion) ) {
+                $AnalyticsRule = $AnalyticsRule | Add-Member -MemberType NoteProperty -Name "version" -Value "1.0.0" -PassThru -Force
+            }
+            # Remove values that are not needed
+            foreach ($RemoveArmValue in $RemoveArmValues) {
+                $AnalyticsRule.PSObject.Properties.Remove($RemoveArmValue) | Out-Null
+            }
+        
+            $JSON = $AnalyticsRule | ConvertTo-Json -Depth 100
+            # Use ISO8601 format for timespan values
+            $JSON = $JSON -replace '"PT([0-9]+)M"', '"$1m"' -replace '"PT([0-9]+)H"', '"$1h"' -replace '"P([0-9]+)D"', '"$1d"'
+        
+            # Convert the names of the properties to the names used in the YAML
+            foreach ($Arm2Yaml in $ValueNameMappingArm2Yaml.Keys) {
+                $JSON = $JSON -replace $Arm2Yaml, $ValueNameMappingArm2Yaml[$Arm2Yaml]
+            }
+        
+            # Convert the compare operators to the names used in the YAML
+            foreach ($Arm2Yaml in $CompareOperatorArm2Yaml.Keys) {
+                $JSON = $JSON -replace $Arm2Yaml, $CompareOperatorArm2Yaml[$Arm2Yaml]
+            }
+        
+            # Convert the JSON to a PowerShell object
+            $AnalyticsRule = $JSON | ConvertFrom-Json
+        
+            # Use custom sort order of YAML
+            $ErrorActionPreference = "SilentlyContinue"
+            $AnalyticsRuleKeys = $AnalyticsRule.PSObject.Properties.Name | Sort-Object { $i = $DefaultSortOrderInYAML.IndexOf($_) ; if ( $i -eq -1 ) { 100 } else { $i } }
+            $ErrorActionPreference = "Continue"
+            # Create ordered hashtable
+            $AnalyticsRuleCleaned = [ordered]@{}
+            foreach ($PropertyName in $AnalyticsRuleKeys) {
+                # Remove empty properties
+                if ( -not [string]::IsNullOrWhiteSpace($AnalyticsRule.$PropertyName) -or ( $AnalyticsRule.$PropertyName -is [array] -and ($AnalyticsRule.$PropertyName.Count -gt 0) ) ) {
+                    $AnalyticsRuleCleaned.Add($PropertyName, $AnalyticsRule.$PropertyName)
+                }
+            }
+        
+            # Convert the PowerShell object to YAML
+            $AnalyticsRuleYAML = $AnalyticsRuleCleaned | ConvertTo-Yaml
+
+            #endregion ART
+
+            # Write the YAML to a file or return the YAML
+            if ($OutFile) {
+                # if (-not (Test-Path $OutFile)) {
+                $AnalyticsRuleYAML | Out-File $OutFile -NoClobber:(-not $Force) -Encoding utf8
+                Write-Verbose "Output written to file: `"$OutFile`""
+                # } else {
+                #     Write-Warning "Duplicate filename Skipping writing to file: `"$OutFile`""
+                #} 
+            } else {
+                $AnalyticsRuleYAML
+            }
+
+            $resourceCounter++
         }
     }
 }
+
